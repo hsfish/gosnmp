4810b6a589f36669b75764c070203ab5945663a0
hsfish <hsfishli@geesunn.com>
2022-11-17 19:28:59 +0800

    优化

--- v3.go
+++ v3.go
@@ -79,12 +79,12 @@ func (packet *SnmpPacket) authenticate(msg []byte) ([]byte, error) {
 	if packet.Version != Version3 {
 		return msg, nil
 	}
-	//if packet.MsgFlags&AuthNoPriv > 0 {
-	//	err := packet.SecurityParameters.authenticate(msg)
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//}
+	if packet.MsgFlags&AuthNoPriv > 0 {
+		err := packet.SecurityParameters.authenticate(msg)
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	return msg, nil
 }
@@ -93,26 +93,26 @@ func (x *GoSNMP) testAuthentication(packet []byte, result *SnmpPacket, useRespon
 	if x.Version != Version3 {
 		return fmt.Errorf("testAuthentication called with non Version3 connection")
 	}
-	msgFlags := x.MsgFlags
-	if useResponseSecurityParameters {
-		msgFlags = result.MsgFlags
-	}
+	//msgFlags := x.MsgFlags
+	//if useResponseSecurityParameters {
+	//	msgFlags = result.MsgFlags
+	//}
 
-	if msgFlags&AuthNoPriv > 0 {
-		var authentic bool
-		var err error
-		if useResponseSecurityParameters {
-			authentic, err = result.SecurityParameters.isAuthentic(packet, result)
-		} else {
-			authentic, err = x.SecurityParameters.isAuthentic(packet, result)
-		}
-		if err != nil {
-			return err
-		}
-		if !authentic {
-			return fmt.Errorf("incoming packet is not authentic, discarding")
-		}
-	}
+	//if msgFlags&AuthNoPriv > 0 {
+	//	var authentic bool
+	//	var err error
+	//	if useResponseSecurityParameters {
+	//		authentic, err = result.SecurityParameters.isAuthentic(packet, result)
+	//	} else {
+	//		authentic, err = x.SecurityParameters.isAuthentic(packet, result)
+	//	}
+	//	if err != nil {
+	//		return err
+	//	}
+	//	if !authentic {
+	//		return fmt.Errorf("incoming packet is not authentic, discarding")
+	//	}
+	//}
 
 	return nil
 }
